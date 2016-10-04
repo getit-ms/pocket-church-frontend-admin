525e7ed05502100a7a841e49a4bd9af3005e8dc1
Gabriel Silva <gafsel@gmail.com>
2016-10-04 12:54:35 -0300

    - ajustando busca de arquivos de ajuda - fechando versão 2.3

--- src/main/webapp/scripts/components/video/video.directive.js
+++ src/main/webapp/scripts/components/video/video.directive.js
@@ -7,9 +7,12 @@ calvinApp.directive('videoPlayer', function(){
         },
         templateUrl: 'scripts/components/video/video.directive.html',
         controller: ['$scope', '$http', '$window', function($scope, $http, $window){
-            $scope.m4v = $scope.media + '.m4v';
-            $scope.webm = $scope.media + '.webm';
-            $scope.jpg = $scope.media + '.jpg';
+                
+            var queryString = '?Igreja=' +$_serverCode + '&Dispositivo=' + $_clientKey + 
+                    '&Authorization='+ localStorage.getItem('Authorization.' + $_serverCode);
+            $scope.m4v = $scope.media + '.m4v' + queryString;
+            $scope.webm = $scope.media + '.webm' + queryString;
+            $scope.jpg = $scope.media + '.jpg' + queryString;
         }]
     };
 }).directive('videoAjuda', function(){
