a3b91d2bb2a9cf1a6fb5890dcf45a6cea6dc5e30
Gabriel Silva <gafsel@gmail.com>
2017-01-30 01:05:49 -0200

    Liberando cancelamento de inscrição

--- src/main/webapp/scripts/app/evento/evento.controller.js
+++ src/main/webapp/scripts/app/evento/evento.controller.js
@@ -139,7 +139,7 @@ calvinApp.config(['$stateProvider', function($stateProvider){
         views:{
             'content@':{
                 templateUrl: 'scripts/app/evento/inscritos.list.html',
-                controller: function(eventoService, $scope, message, NgTableParamsCalvin, $stateParams){
+                controller: function(eventoService, $scope, message, NgTableParamsCalvin, $stateParams, confirmDialog){
                     $scope.filtro = {};
                     
                     $scope.tabelaInscricoes = new NgTableParamsCalvin(function($defer, params){
@@ -154,6 +154,23 @@ calvinApp.config(['$stateProvider', function($stateProvider){
                     
                     $scope.busca = function(){
                         $scope.tabelaInscricoes.reload();
+                    };
+                    
+                    $scope.cancelar = function(inscricao){
+                        confirmDialog({
+                            title:'evento.cancelamento_inscricao',
+                            text:'mensagens.MSG-045',
+                            ok:'global.sim',
+                            cancel:'global.nao',
+                            params:{
+                                inscrito: inscricao.nomeInscrito
+                            }
+                        }).then(function(){
+                            eventoService.cancelaInscricao($stateParams.id, inscricao.id, function(inscricao){
+                                message({type:'success',body:'mensagens.MSG-001'});
+                                $scope.busca();
+                            });
+                        });
                     };
                     
                     $scope.confirmar = function(inscricao){
