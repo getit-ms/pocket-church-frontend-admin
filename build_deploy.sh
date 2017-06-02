b3f667d7d676bb77aee1d606b67aa88139787b31
Gabriel Silva <gafsel@gmail.com>
2017-06-01 23:40:11 -0300

    Ajuste no pipeline

--- build_deploy.sh
+++ build_deploy.sh
@@ -5,8 +5,8 @@ git clean -fd || exit 1
 
 cp -Rf /tmp/calvinista-design/$1/admin/* src/main/webapp/ || exit 1
 
-mvn package -Pprod -Dchave.igreja=$1 -Dnome.igreja="$2" || exit 1
+sudo mvn package -Pprod -Dchave.igreja=$1 -Dnome.igreja="$2" || exit 1
 
-sshpass -p "$DEPLOY_PASS" scp -o StrictHostKeyChecking=no "target/$1.war" "$DEPLOY_USER@$DEPLOY_HOST:/tmp/" || exit 1
-sshpass -p "$DEPLOY_PASS" ssh -o StrictHostKeyChecking=no "$DEPLOY_USER@$DEPLOY_HOST" "/opt/glassfish4/glassfish/bin/asadmin undeploy $1 || echo 'Não implantado'" || echo "$1 não estava em execução"
-sshpass -p "$DEPLOY_PASS" ssh -o StrictHostKeyChecking=no "$DEPLOY_USER@$DEPLOY_HOST" "/opt/glassfish4/glassfish/bin/asadmin deploy /tmp/$1.war || exit 1"
+sudo sshpass -p "$DEPLOY_PASS" scp -o StrictHostKeyChecking=no "target/$1.war" "$DEPLOY_USER@$DEPLOY_HOST:/tmp/" || exit 1
+sudo sshpass -p "$DEPLOY_PASS" ssh -o StrictHostKeyChecking=no "$DEPLOY_USER@$DEPLOY_HOST" "/opt/glassfish4/glassfish/bin/asadmin undeploy $1 || echo 'Não implantado'" || echo "$1 não estava em execução"
+sudo sshpass -p "$DEPLOY_PASS" ssh -o StrictHostKeyChecking=no "$DEPLOY_USER@$DEPLOY_HOST" "/opt/glassfish4/glassfish/bin/asadmin deploy /tmp/$1.war || exit 1"
