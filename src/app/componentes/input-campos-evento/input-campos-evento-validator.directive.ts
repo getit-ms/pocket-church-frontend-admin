5471146196c18c2142c15f6e3ec93e57f20798c3
Gabriel Silva <gafsel@gmail.com>
2020-11-10 08:12:03 -0300

    Adicionando validação para evitar nomes repetidos em campos

--- src/app/componentes/input-campos-evento/input-campos-evento-validator.directive.ts
+++ src/app/componentes/input-campos-evento/input-campos-evento-validator.directive.ts
@@ -1,25 +1,33 @@
-import { Directive } from '@angular/core';
+import {Directive} from '@angular/core';
 import {AbstractControl, ControlValueAccessor, NG_VALIDATORS, ValidationErrors, Validator} from "@angular/forms";
 
 @Directive({
-  selector: '[appInputCamposEventoValidator],app-input-campos-evento',
-  providers: [{
-    provide: NG_VALIDATORS,
-    useExisting: InputCamposEventoValidatorDirective,
-    multi: true
-  }]
+    selector: '[appInputCamposEventoValidator],app-input-campos-evento',
+    providers: [{
+        provide: NG_VALIDATORS,
+        useExisting: InputCamposEventoValidatorDirective,
+        multi: true
+    }]
 })
 export class InputCamposEventoValidatorDirective implements Validator {
 
-  constructor() { }
-
-  validate(control: AbstractControl): ValidationErrors | null {
-    if (control.value && control.value.length) {
-      if (control.value.find(campo => !campo.nome)) {
-        return {campoSemNome: true};
-      }
+    constructor() {
     }
 
-    return undefined;
-  }
+    validate(control: AbstractControl): ValidationErrors | null {
+        if (control.value && control.value.length) {
+            if (control.value.find(campo => !campo.nome)) {
+                return {campoSemNome: true};
+            }
+
+            for (const campo of control.value) {
+                if (control.value.find(outro => outro !== campo &&
+                    outro.nome.toLowerCase() === campo.nome.toLowerCase())) {
+                    return {campoNomeRepetido: {nome: campo.nome}};
+                }
+            }
+        }
+
+        return undefined;
+    }
 }
